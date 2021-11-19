ca804db81d0e3ccba2bd3068f43321773e4199f5
Theodore Frazier <theo.frazier@icloud.com>
2021-11-19 12:27:44 -0500

    planning complete - built constructor to access data and created an array to recieve cart products, refactored productData page to be able to assess data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+import { Component } from 'react';
 import "./App.css";
+import data from './data/productData.js'
 
-const App = () => {
-  return <h1>Hello, world!</h1>;
-};
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      garageSaleList: data.productData,
+      cartProducts: [],
+      
+
+    }
+  }
+
+  
+
+
+
+
+  render() {
+
+    return (
+      <div>
+        <h1>Hello, world!!</h1>
+      </div>
+    )
+  };
+
+}
+
+
+
+
+
 
 export default App;
